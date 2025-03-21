964b64f17ac4a6598e6d40bd020645df8d3c6039
Christopher Biel <christopher.biel@biels.net>
2025-03-21 14:55:29 +0100

    corrected Flask app template path

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from backend.config import Config
 
-app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))  # Ensure Flask finds templates
 app.config.from_object(Config)
 
 # Import and register routes
