97e3271904e76746bc9081977a2f64dc888ebf5e
Christopher Biel <christopher.biel@tum.de>
2026-02-11 16:47:21 -0400

    Add Docker Compose and Caddy setup

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,11 +1,13 @@
+from .health import health_bp
 from .media import media_bp
 from .questions import questions_bp
 from .users import users_bp
 
-__all__ = ["questions_bp", "users_bp", "media_bp"]
+__all__ = ["questions_bp", "users_bp", "media_bp", "health_bp"]
 
 
 def init_routes(app):
     app.register_blueprint(questions_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(media_bp)
+    app.register_blueprint(health_bp)
